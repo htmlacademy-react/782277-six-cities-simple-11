297574ac90bf1326768e72a1929309808026a508
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-10-24 16:19:18 +0300

    Добавит react-компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+enum Setting {
+  cardCount = 5
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardCount={Setting.cardCount} />
   </React.StrictMode>,
 );
---
+++ project/src/pages/offer-page/offer-page.tsx
@@ -0,0 +1,40 @@
+import Header from '../../components/header/header';
+import UserNavigation from '../../components/user-navigation/user-navigation';
+import Gallery from '../../components/gallery/gallery';
+import Offer from '../../components/offer/offer';
+import Review from '../../components/review/review';
+import OfferCard from '../../components/offer-card/offer-card';
+
+export default function OfferPage(): JSX.Element {
+  return (
+    <div className="page">
+      <Header>
+        <UserNavigation />
+      </Header>
+
+      <main className="page__main page__main--property">
+        <section className="property">
+          <Gallery />
+          <div className="property__container container">
+            <div className="property__wrapper">
+              <Offer />
+              <Review />
+            </div>
+          </div>
+          <section className="property__map map"></section>
+        </section>
+        <div className="container">
+          <section className="near-places places">
+            <h2 className="near-places__title">Other places in the neighbourhood</h2>
+            <div className="near-places__list places__list">
+              {/* TODO при появлении данных, переделать на map */}
+              <OfferCard />
+              <OfferCard />
+              <OfferCard />
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
