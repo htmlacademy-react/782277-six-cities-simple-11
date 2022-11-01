37478dcc8ada8f596d99261dab033b8caa6de9c0
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-01 12:08:41 +0300

    Добавит управляемые поля в форму отзыва

--- project/src/components/offer-review/offer-review.tsx
+++ project/src/components/offer-review/offer-review.tsx
@@ -1,5 +1,6 @@
 import {calculateRatingWidth, formatDate} from '../../utils';
 import {Reviews} from '../../types/review';
+import ReviewForm from '../review-form/review-form';
 
 type OfferReviewProps = {
   reviews: Reviews;
@@ -14,88 +15,39 @@ export default function OfferReview({reviews}: OfferReviewProps): JSX.Element {
       </h2>
 
       <ul className="reviews__list">
-        {reviews && reviews.map((review) => {
-          const {id, user, rating, comment, date} = review;
-
-          return (
-            <li className="reviews__item" key={id}>
-              <div className="reviews__user user">
-                <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                  <img
-                    className="reviews__avatar user__avatar"
-                    src={user.avatarUrl}
-                    width="54"
-                    height="54"
-                    alt={user.name}
-                  />
-                </div>
-                <span className="reviews__user-name">
-                  {user.name}
-                </span>
+        {reviews && reviews.map((review) => (
+          <li className="reviews__item" key={review.id}>
+            <div className="reviews__user user">
+              <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                <img
+                  className="reviews__avatar user__avatar"
+                  src={review.user.avatarUrl}
+                  width="54"
+                  height="54"
+                  alt={review.user.name}
+                />
               </div>
-              <div className="reviews__info">
-                <div className="reviews__rating rating">
-                  <div className="reviews__stars rating__stars">
-                    <span style={{width: calculateRatingWidth(rating)}}></span>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
+              <span className="reviews__user-name">
+                {review.user.name}
+              </span>
+            </div>
+            <div className="reviews__info">
+              <div className="reviews__rating rating">
+                <div className="reviews__stars rating__stars">
+                  <span style={{width: calculateRatingWidth(review.rating)}}></span>
+                  <span className="visually-hidden">Rating</span>
                 </div>
-                <p className="reviews__text">
-                  {comment}
-                </p>
-                <time className="reviews__time" dateTime={date}>{formatDate(date)}</time>
               </div>
-            </li>
-          );
-        })}
+              <p className="reviews__text">
+                {review.comment}
+              </p>
+              <time className="reviews__time" dateTime={review.date}>{formatDate(review.date)}</time>
+            </div>
+          </li>
+        ))}
       </ul>
 
-      <form className="reviews__form form" action="#" method="post">
-        <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating">
-          <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-        </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
-            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-        </div>
-      </form>
+      <ReviewForm />
     </section>
   );
 }
--- project/src/const.ts
+++ project/src/const.ts
@@ -15,3 +15,11 @@ export enum PHOTO_URL {
   Avatar = 'https://i.pravatar.cc/74?rnd=',
   Offer = 'http://picsum.photos/260/200?r='
 }
+
+export const GRADES: string[] = [
+  'perfect',
+  'good',
+  'not bad',
+  'badly',
+  'terribly'
+];
