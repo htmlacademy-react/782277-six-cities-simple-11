0d3ef76553955ba8450b96f0e97f8de3a35ffbdf
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-22 12:45:05 +0300

    Добавит модуль для работы с token

---
+++ project/src/services/token.ts
@@ -0,0 +1,16 @@
+const AUTHORIZATION_TOKEN_KEY_NAME = 'six-cities-token';
+
+type Token = string;
+
+export const getToken = (): Token => {
+  const token = localStorage.getItem(AUTHORIZATION_TOKEN_KEY_NAME);
+  return token ?? '';
+};
+
+export const saveToken = (token: Token): void => {
+  localStorage.setItem(AUTHORIZATION_TOKEN_KEY_NAME, token);
+};
+
+export const removeToken = (): void => {
+  localStorage.removeItem(AUTHORIZATION_TOKEN_KEY_NAME);
+};
