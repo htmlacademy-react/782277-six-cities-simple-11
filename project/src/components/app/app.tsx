b20c522b944681730490070db99c67f43c6593a0
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-25 10:34:26 +0300

    Подключит offer-page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,27 +1,16 @@
-import {useEffect} from 'react';
 import HistoryRouter from '../history-route/history-route';
 import {Routes, Route} from 'react-router-dom';
 import {browserHistory} from '../../browser-history';
 import {HelmetProvider} from 'react-helmet-async';
 
-import {checkAuthorizationAction, fetchOfferAction} from '../../store/api-action';
-import {useAppDispatch} from '../../hooks/useAppDispatch';
-
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
-// import OfferPage from '../../pages/offer-page/offer-page';
+import OfferPage from '../../pages/offer-page/offer-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 import {AppRoute} from '../../const';
 
 export default function App(): JSX.Element {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(checkAuthorizationAction());
-    dispatch(fetchOfferAction());
-  }, [dispatch]);
-
   return (
     <HelmetProvider>
       <HistoryRouter history={browserHistory}>
@@ -34,16 +23,10 @@ export default function App(): JSX.Element {
             path={AppRoute.Login}
             element={<LoginPage />}
           />
-          {/* <Route
+          <Route
             path={`${AppRoute.Offer}/:id`}
-            element={
-              <OfferPage
-                offers={offers}
-                nearOffers={nearOffers}
-                allReviews={allReviews}
-              />
-            }
-          /> */}
+            element={<OfferPage />}
+          />
           <Route
             path={AppRoute.NotFound}
             element={<NotFoundPage />}
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 
 import {store} from './store/store';
+import {checkAuthorizationAction, fetchOfferAction} from './store/api-action';
+
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
+
+store.dispatch(checkAuthorizationAction());
+store.dispatch(fetchOfferAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
