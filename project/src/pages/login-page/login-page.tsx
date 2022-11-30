5b2aab7cd3dc884e11f0f1b182bdd21035e071fe
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-11-30 10:47:35 +0300

    Добавит редирект на главную

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,10 +1,23 @@
 import {Helmet} from 'react-helmet-async';
+import {useNavigate} from 'react-router-dom';
+
+import {useAppSelector} from '../../hooks/useAppSelector';
+import {getAuthorizationStatus} from '../../store/user-data/selectors';
 
 import Header from '../../components/header/header';
 import LoginForm from '../../components/login-form/login-form';
 
+import {AppRoute, AuthorizationStatus} from '../../const';
+
 
 function LoginPage(): JSX.Element {
+  const navigate = useNavigate();
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+
+  if (authorizationStatus === AuthorizationStatus.Authorized) {
+    navigate(AppRoute.Main);
+  }
+
   return (
     <div className="page page--gray page--login">
       <Helmet>
