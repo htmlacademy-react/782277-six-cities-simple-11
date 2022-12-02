dea4817c52638ef9375b5d536ec8afb278a44ab1
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-12-02 15:38:10 +0300

    Фиксит названия тест-кейсов

--- project/src/store/user-data/user-data.test.ts
+++ project/src/store/user-data/user-data.test.ts
@@ -22,7 +22,7 @@ describe('Reducer: userData', () => {
       .toEqual(state);
   });
 
-  describe('checkAuthorizationAction', () => {
+  describe('Action: checkAuthorizationAction', () => {
     it('should update authorizationStatus to "AUTHORIZED" and return "userData" if checkAuthAction fulfilled', () => {
       expect(userData.reducer(state, {type: checkAuthorizationAction.fulfilled.type, payload: fakeUserData}))
         .toEqual({authorizationStatus: AuthorizationStatus.Authorized, userData: fakeUserData});
@@ -34,7 +34,7 @@ describe('Reducer: userData', () => {
     });
   });
 
-  describe('loginAction', () => {
+  describe('Action: loginAction', () => {
     it('should update authorizationStatus to "AUTHORIZED" and return "userData" if loginAction fulfilled', () => {
       expect(userData.reducer(state, {type: loginAction.fulfilled.type, payload: fakeUserData}))
         .toEqual({authorizationStatus: AuthorizationStatus.Authorized, userData: fakeUserData});
@@ -45,7 +45,7 @@ describe('Reducer: userData', () => {
     });
   });
 
-  describe('logoutAction', () => {
+  describe('Action: logoutAction', () => {
     it('should update authorizationStatus to "NO_AUTHORIZED" if logoutAction fulfilled', () => {
       expect(userData.reducer(state, {type: logoutAction.fulfilled.type}))
         .toEqual({authorizationStatus: AuthorizationStatus.NoAuthorized, userData: {}});
