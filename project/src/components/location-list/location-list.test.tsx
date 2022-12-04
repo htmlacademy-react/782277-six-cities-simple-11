c011512da679eebc5ae089f2df958f83f1cefe52
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-12-04 14:08:12 +0300

    Добавит тест LocationList

---
+++ project/src/components/location-list/location-list.test.tsx
@@ -0,0 +1,42 @@
+import {render, screen} from '@testing-library/react';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {Provider} from 'react-redux';
+import {HelmetProvider} from 'react-helmet-async';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import LocationList from './location-list';
+import {makeFakeOffers} from '../../utils/mocks';
+import {Location, SortType} from '../../const';
+
+const fakeOffers = makeFakeOffers();
+
+const fakeState = {
+  OFFERS: {
+    location: Location.Paris,
+    sortType: SortType.Popular,
+    offers: fakeOffers,
+    selectedOffer: fakeOffers[0],
+    isOffersLoading: false
+  }
+};
+
+const mockStore = configureMockStore();
+const history = createMemoryHistory();
+
+describe('Component: LocationList', () => {
+  it('should render correctly', () => {
+    const store = mockStore(fakeState);
+
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <LocationList />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByTestId('location-list')).toBeInTheDocument();
+  });
+});
