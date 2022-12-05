18b43c4f94d4a33dc401365a1c1f791ea0a30ebf
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-12-05 12:01:55 +0300

    Добавит тест hook useMap

---
+++ project/src/hooks/useMap.test.tsx
@@ -0,0 +1,35 @@
+import {renderHook} from '@testing-library/react';
+import {useRef} from 'react';
+import {Map} from 'leaflet';
+import useMap from './useMap';
+import {makeFakeOffer} from '../utils/mocks';
+
+const fakeOffer = makeFakeOffer();
+const mokeLocation = fakeOffer.location;
+
+describe('Hook: useMap', () => {
+  it('should return instance of leaflet if it has ref and location', () => {
+    const blockElement = document.createElement('div');
+
+    const {result} = renderHook(() => {
+      const mokeMapRef = useRef<HTMLElement | null>(blockElement);
+
+      return useMap(mokeMapRef, mokeLocation);
+    });
+
+    expect(result.current).not.toBeNull();
+    expect(result.current).toBeInstanceOf(Object);
+    expect(result.current).toBeInstanceOf(Map);
+  });
+
+  it('should return null if it has not ref', () => {
+    const {result} = renderHook(() => {
+      const mokeMapRef = useRef<HTMLElement | null>(null);
+
+      return useMap(mokeMapRef, mokeLocation);
+    });
+
+    expect(result.current).toBeNull();
+    expect(result.current).not.toBeInstanceOf(Map);
+  });
+});
